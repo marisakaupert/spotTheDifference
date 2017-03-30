321975d03ea90d467cf3313e4fc799d0e4ab9b48
Marisa Lozano <marisalozano1995@gmail.com>
2017-03-30 16:51:21 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+	frontend:{
+		sass: {
+			src: './assets/scss/**/*.scss',
+			dest: './assets/css'
+		},
+		js: {
+			src: ['./assets/js/**/*.js', '!./assets/js/app.min.js', '!./assets/js/libraries/**/*.js'],
+			dest: './assets/js/',
+			filename:'app.min.js'
+		}
+	}
+};
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: [__dirname],
+    port: process.env.PORT || 8000, // localhost:5000
+    livereload: false
+  });
+});
+
+
+gulp.task('scripts', function() {
+	console.log('Concatenating Scripts');
+	return gulp.src(paths.frontend.js.src)
+		.pipe(sourcemaps.init())
+		.pipe(concat(paths.frontend.js.filename))
+		.pipe(uglify().on('error',handleError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.frontend.js.dest));
+});
+
+gulp.task('styles', function(){
+	console.log('Compiling Sass');
+	gulp.src(paths.frontend.sass.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'expanded'}).on('error',handleError))
+		.pipe(sourcemaps.write())
+		.pipe(prefix("last 2 versions", "> 1%", "ie 8", "ie 7"))
+		.pipe(gulp.dest(paths.frontend.sass.dest));
+});
+
+gulp.task('default', ['serveprod', 'styles', 'scripts'], function(){
+	gulp.watch(paths.frontend.sass.src, ['styles']);
+	gulp.watch(paths.frontend.js.src, ['scripts']);
+});
+
+function handleError(error){
+	console.log(error.toString());
+	this.emit('end');
+}
