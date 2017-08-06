ca85a1214b5f0647846f6b7ee799639d85acbd97
marisaslozano <marisalozano1995@gmail.com>
2017-08-06 13:42:19 -0400

    test js

--- assets/js/global.js
+++ assets/js/global.js
@@ -111,7 +111,7 @@ var spotted = function() {
 
 
 var win = function() {
-  $('#background' + background + ' h2').runner('clear');
+  $('#background' + background + ' h2').runner('reset', true);
   $('#background' + background).hide();
   $('#endWinScreen').show();
   if (timeoutID !== null) {
@@ -125,7 +125,7 @@ var win = function() {
 
 
 var lose = function() {
-  $('#background' + background + ' h2').runner('clear');
+  $('#background' + background + ' h2').runner('reset', true);
   $('#background' + background).hide();
   $('#endLoseScreen').show();
   if (timeoutID !== null) {
